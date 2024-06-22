b9e07b68c3b5cf28a7dd7eb50f7574e7e35909da
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-22 18:18:22 +0800

    add ThemeContextProvider + hook

---
+++ context/ThemeContextProvider.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import useLocalStorage from "@/hooks/useLocalStorage"
+import React, { createContext, useEffect } from "react"
+
+export type TThemeContext = {
+  theme: "light" | "dark"
+  handleThemeChange: () => void
+}
+export const ThemeContext = createContext<TThemeContext | null>(null)
+
+const ThemeContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useLocalStorage<TThemeContext["theme"]>({
+    key: "devflow-theme",
+    initialValue: "light",
+  })
+
+  const handleThemeChange = () => {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [theme])
+
+  console.log("theme", theme)
+
+  return (
+    <ThemeContext.Provider value={{ theme, handleThemeChange }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeContextProvider
