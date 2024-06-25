1d57fc1fb879cb813e7281fc4632b2931b99c4a0
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-25 16:00:48 +0800

    add utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
 import { type ClassValue, clsx } from "clsx"
+import { differenceInDays } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const getDaysAgo = (createdAt: Date) =>
+  differenceInDays(new Date(), createdAt)
+
+export function formatNumber(number: number) {
+  if (number >= 1000000) {
+    return (number / 1000000).toFixed(1) + "M"
+  } else if (number >= 1000) {
+    return (number / 1000).toFixed(1) + "K"
+  } else {
+    return number.toString()
+  }
 }
