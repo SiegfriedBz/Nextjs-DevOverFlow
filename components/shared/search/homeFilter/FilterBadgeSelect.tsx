bee19e970df18f58c6b5107d9a63e07992aa1329
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-25 10:56:35 +0800

    add filter components

---
+++ components/shared/search/homeFilter/FilterBadgeSelect.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import React from "react"
+
+type TProps = {
+  filter: string
+  setFilter: React.Dispatch<React.SetStateAction<string>>
+  options: string[]
+  wrapperClassName?: string
+  className?: string
+}
+
+const FilterBadgeSelect = ({
+  filter,
+  setFilter,
+  options,
+  wrapperClassName,
+  className,
+}: TProps) => {
+  console.log("FilterBadgeSelect filter", filter)
+  const activeOption = filter
+
+  return (
+    <ul className={wrapperClassName}>
+      {options.map((option) => {
+        return (
+          <Button onClick={() => setFilter(option)} key={`badge-${option}`}>
+            <Badge
+              className={`body-medium 
+                  ${className} 
+                  ${
+                    activeOption === option
+                      ? "bg-primary-100 text-primary-500"
+                      : "bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-200"
+                  }  body-regular light-border flex items-center justify-center rounded-xl border-none px-4 py-2.5`}
+            >
+              {option}
+            </Badge>
+          </Button>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default FilterBadgeSelect
---
+++ components/shared/search/homeFilter/FilterCustomSelect.tsx
@@ -0,0 +1,55 @@
+"use client"
+import {
+  Select,
+  SelectContent,
+  // SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import React from "react"
+
+type TProps = {
+  filter: string
+  setFilter: React.Dispatch<React.SetStateAction<string>>
+  options: string[]
+  wrapperClassName?: string
+  className?: string
+}
+
+const FilterCustomSelect = ({
+  filter,
+  setFilter,
+  options,
+  wrapperClassName,
+  className,
+}: TProps) => {
+  console.log("FilterCustomSelect filter", filter)
+
+  return (
+    <div className={wrapperClassName}>
+      <Select onValueChange={setFilter}>
+        <SelectTrigger
+          className={`${className} text-dark500_light700 body-regular light-border background-light800_dark300 w-full rounded-xl border-none px-4 py-2.5`}
+        >
+          <div className="line-clamp-1 flex-1 text-left">
+            <SelectValue placeholder="Select a filter" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          {/* <SelectGroup> */}
+          {options.map((option) => {
+            return (
+              <SelectItem key={`option-${option}`} value={option}>
+                {option}
+              </SelectItem>
+            )
+          })}
+          {/* </SelectGroup> */}
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
+
+export default FilterCustomSelect
