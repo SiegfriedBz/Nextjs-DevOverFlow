af2f18ca4def00788bbc1abf5c74bdb2affe21b1
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-27 11:07:22 +0800

    refactor Home with a Suspense boundary around QuestionListWrapper async component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,119 +1,15 @@
 import QuestionCard from "@/components/QuestionCard"
+import QuestionCardSkeleton from "@/components/QuestionCardSkeleton"
 import NoResult from "@/components/shared/NoResult"
 import HomeFilter from "@/components/shared/search/homeFilter/HomeFilter"
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar"
 import { Button } from "@/components/ui/button"
-import type { TQuestion } from "@/types"
+import { getAllQuestions } from "@/services/question.services"
+import type { TQuestion, TSearchParamsProps } from "@/types"
 import Link from "next/link"
+import { Suspense } from "react"
 
-export type TTag = {
-  _id: number
-  name: string
-  totalQuestions?: number
-  showCount?: boolean
-}
-const QUESTIONS: TQuestion[] = [
-  {
-    _id: "1",
-    title: "What is TypeScript and how does it differ from JavaScript?",
-    tags: [
-      { _id: "1", name: "TypeScript" },
-      { _id: "2", name: "JavaScript" },
-    ],
-    author: {
-      _id: "a1",
-      name: "Alice Johnson",
-      picture: "",
-    },
-    createdAt: new Date("2023-01-15T08:00:00Z"),
-    numOfVotes: 10000000,
-    numOfViews: 1500,
-    answers: [],
-  },
-  {
-    _id: "2",
-    title: "How do you manage state in a React application?",
-    tags: [
-      { _id: "3", name: "React" },
-      { _id: "4", name: "State Management" },
-    ],
-    author: {
-      _id: "a2",
-      name: "Bob Smith",
-      picture: "",
-    },
-    createdAt: new Date("2023-02-20T09:30:00Z"),
-    numOfVotes: 25,
-    numOfViews: 300,
-    answers: [],
-  },
-  {
-    _id: "3",
-    title: "What are the benefits of using GraphQL over REST?",
-    tags: [
-      { _id: "5", name: "GraphQL" },
-      { _id: "6", name: "REST" },
-    ],
-    author: {
-      _id: "a3",
-      name: "Charlie Brown",
-      picture: "",
-    },
-    createdAt: new Date("2023-03-10T10:15:00Z"),
-    numOfVotes: 15,
-    numOfViews: 250,
-    answers: [],
-  },
-  {
-    _id: "4",
-    title: "How does the virtual DOM work in React?",
-    tags: [
-      { _id: "3", name: "React" },
-      { _id: "7", name: "Virtual DOM" },
-    ],
-    author: {
-      _id: "a4",
-      name: "Diana Prince",
-      picture: "",
-    },
-    createdAt: new Date("2023-04-05T11:45:00Z"),
-    numOfVotes: 30,
-    numOfViews: 400,
-    answers: [],
-  },
-  {
-    _id: "5",
-    title: "What are the main features of Node.js?",
-    tags: [
-      { _id: "8", name: "Node.js" },
-      { _id: "9", name: "JavaScript" },
-    ],
-    author: {
-      _id: "a5",
-      name: "Ethan Hunt",
-      picture: "",
-    },
-    createdAt: new Date("2023-05-01T12:00:00Z"),
-    numOfVotes: 20,
-    numOfViews: 350,
-    answers: [],
-  },
-]
-
-type TSearchParams = {
-  searchParams: { [key: string]: string | undefined }
-}
-
-const getData = async ({ searchParams }: { searchParams: TSearchParams }) => {
-  // const response = await getQuestions({ searchParams })
-  return []
-}
-
-const Home = async ({ searchParams }: { searchParams: TSearchParams }) => {
-  const data = await getData({ searchParams })
-  console.log(data)
-  console.log("HOME searchParams", searchParams)
-
+const Home = ({ searchParams }: TSearchParamsProps) => {
   return (
     <div>
       <div className="flex w-full items-center max-sm:flex-col-reverse sm:justify-between">
@@ -133,22 +29,46 @@ const Home = async ({ searchParams }: { searchParams: TSearchParams }) => {
       </div>
 
       <div className="mt-4 flex flex-col justify-between gap-8 sm:items-center">
-        {QUESTIONS?.length > 0 ? (
-          QUESTIONS.map((question) => {
-            return <QuestionCard key={question._id} {...question} />
-          })
-        ) : (
-          <NoResult
-            resultType="question"
-            paragraphContent="Be the first to break the silence! Ask a question and kickstart a
-            discussion"
-            href="/ask-a-question"
-            linkLabel="Ask a question"
-          />
-        )}
+        <Suspense fallback={<QuestionListWrapperSkeleton />}>
+          <QuestionListWrapper searchParams={searchParams} />
+        </Suspense>
       </div>
     </div>
   )
 }
 
 export default Home
+
+const wait = (delay: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, delay)
+  })
+
+const QuestionListWrapper = async ({ searchParams }: TSearchParamsProps) => {
+  const data: TQuestion[] | null = await getAllQuestions({ searchParams })
+  console.log(data)
+  console.log("==== HOME searchParams", searchParams)
+  console.log("==== HOME data", data)
+
+  await wait(2500)
+
+  return data && data?.length > 0 ? (
+    data.map((question) => {
+      return <QuestionCard key={question._id} {...question} />
+    })
+  ) : (
+    <NoResult
+      resultType="question"
+      paragraphContent="Be the first to break the silence! Ask a question and kickstart a
+            discussion"
+      href="/ask-a-question"
+      linkLabel="Ask a question"
+    />
+  )
+}
+
+const QuestionListWrapperSkeleton = () => {
+  return Array.from({ length: 8 }, (_, index) => {
+    return <QuestionCardSkeleton key={index} />
+  })
+}
