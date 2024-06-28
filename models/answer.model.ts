e450f38eea125f9d68fbb950a105627a837a5032
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-28 16:52:01 +0800

    update Answer model

--- models/answer.model.ts
+++ models/answer.model.ts
@@ -3,15 +3,20 @@ import { model, models, Schema, Types } from "mongoose"
 export interface IAnswerDocument extends Document {
   author: Schema.Types.ObjectId // User
   content: string
+  createdAt: Date
+  upVoters: Schema.Types.ObjectId[] // User
+  downVoters: Schema.Types.ObjectId[] // User
+  question: Schema.Types.ObjectId // Question
 }
 
-const answerSchema = new Schema<IAnswerDocument>(
-  {
-    author: { type: Types.ObjectId, ref: "User" },
-    content: String,
-  },
-  { timestamps: true }
-)
+const answerSchema = new Schema<IAnswerDocument>({
+  author: { type: Types.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+  question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+  createdAt: { type: Date, default: Date.now },
+  upVoters: [{ type: Types.ObjectId, ref: "User" }],
+  downVoters: [{ type: Types.ObjectId, ref: "User" }],
+})
 
 const Answer = models.Answer || model<IAnswerDocument>("Answer", answerSchema)
 
