3ec2678e63a298e75fc2a5e5109619e0e14e299f
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-28 19:20:37 +0800

    fetch user from QuestionDetailsPage + refactor

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,19 +1,23 @@
 import AnswerForm from "@/components/forms/AnswerForm"
+import SaveQuestionButton from "@/components/SaveQuestionButton"
 import Metric from "@/components/shared/Metric"
 import NoResult from "@/components/shared/NoResult"
 import ParsedHtml from "@/components/shared/ParsedHtml"
 import CustomFilter from "@/components/shared/search/filter/CustomFilter"
 import Tag from "@/components/shared/Tag"
-import { Button } from "@/components/ui/button"
 import Voting from "@/components/Voting"
 import { ANSWERS_FILTER_OPTIONS } from "@/constants/filters"
 import { formatDate, getDaysAgo } from "@/lib/dates.utils"
+import { IUserDocument } from "@/models/user.model"
 import { getAllAnswers } from "@/services/answer.services"
 import { getQuestion } from "@/services/question.services"
+import { getUser } from "@/services/user.services"
 import type { TAnswer, TQuestion, TTag, TUser } from "@/types"
 import Image from "next/image"
 import Link from "next/link"
 import React, { Suspense } from "react"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
 
 type TProps = {
   params: { id: string }
@@ -22,9 +26,24 @@ type TProps = {
 const QuestionDetailsPage = async ({ params, searchParams }: TProps) => {
   const { id: questionId } = params
 
-  // We do not populate the question to fetch all answers.
-  // Instead, we use getAllAnswers service to which we can pass searchParams.
-  const question: TQuestion = await getQuestion({ filter: { _id: questionId } })
+  // Get current clerk user
+  const user = await currentUser()
+
+  if (!user) {
+    redirect("/sign-in")
+  }
+
+  const [question, mongoUser] = (await Promise.all([
+    // We do not populate the question to fetch all answers.
+    // Instead, we use getAllAnswers service to which we can pass searchParams.
+    getQuestion({ filter: { _id: questionId } }),
+    // Get user from mongodb
+    getUser({ filter: { clerkId: user.id } }),
+  ])) as [question: TQuestion, mongoUser: IUserDocument]
+
+  const currentUserMongoId = mongoUser?._id as string
+  const currentUserSavedQuestions =
+    mongoUser?.savedQuestions as unknown as string[] // non-populated savedQuestions
 
   const {
     // _id: questionId,
@@ -53,15 +72,12 @@ const QuestionDetailsPage = async ({ params, searchParams }: TProps) => {
         numUpVotes={numUpVotes}
         numDownVotes={numDownVotes}
       >
-        <Button className="p-0">
-          <Image
-            src="/assets/icons/star.svg"
-            alt="star"
-            width={24}
-            height={24}
-            className="invert-colors"
-          />
-        </Button>
+        {/* Client-Component */}
+        <SaveQuestionButton
+          questionId={questionId}
+          currentUserMongoId={currentUserMongoId}
+          currentUserSavedQuestions={currentUserSavedQuestions}
+        />
       </Header>
 
       {/* Question title */}
