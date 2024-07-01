cee20aa284867ca77e48fea3be28d519e3c1a25d
siegfriedbz <siegfried.bozza@yahoo.com>
2024-07-01 14:04:39 +0800

    fix import

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,6 +1,5 @@
+import { cn } from "@/lib/shadcn.utils"
 import * as React from "react"
-
-import { cn } from "@/lib/utils"
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
