86f3c1ca3df0b3cce39c18b661ac3ca237726dd4
siegfriedbz <siegfried.bozza@yahoo.com>
2024-07-04 19:54:15 +0800

    add actionType to Interaction model

--- models/interaction.model.ts
+++ models/interaction.model.ts
@@ -2,19 +2,23 @@ import { Document, model, models, Schema } from "mongoose"
 
 export interface IInteractionDocument extends Document {
   user: Schema.Types.ObjectId
-  actionType: "view" | "like"
+  actionType: "view" | "like" | "ask_question" | "answer"
   question?: Schema.Types.ObjectId
   answer?: Schema.Types.ObjectId
-  tag?: Schema.Types.ObjectId
+  tags?: Schema.Types.ObjectId[]
   createdAt: Date
 }
 
 const interactionSchema = new Schema<IInteractionDocument>({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  actionType: { type: String, enum: ["view", "like"], required: true },
+  actionType: {
+    type: String,
+    enum: ["view", "like", "ask_question", "answer"],
+    required: true,
+  },
   question: { type: Schema.Types.ObjectId, ref: "Question" },
   answer: { type: Schema.Types.ObjectId, ref: "Answer" },
-  tag: { type: Schema.Types.ObjectId, ref: "Tag" },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
   createdAt: { type: Date, default: Date.now },
 })
 
