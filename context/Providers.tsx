bd5bb463184d242dfa325118fd9aedd91c08658f
siegfriedbz <siegfried.bozza@yahoo.com>
2024-07-05 15:08:22 +0800

    fix toast css

--- context/Providers.tsx
+++ context/Providers.tsx
@@ -12,18 +12,15 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
       <Toaster
         position="top-center"
         toastOptions={{
-          unstyled: true,
-          style: {
-            zIndex: 9999,
-            border: "1px solid #FF7000",
-            borderRadius: "5px",
-          },
           className: "class",
           classNames: {
-            error: "bg-red-500 flex items-center w-fit px-4 py-2",
-            success: "text-primary-500 flex items-center w-fit px-4 py-2",
-            warning: "text-yellow-400 flex items-center w-fit px-4 py-2",
-            info: "bg-blue-400 flex items-center  w-fit px-4 py-2",
+            error:
+              "opacity-100 z-[9999] bg-red-500 flex items-center w-fit px-4 py-2",
+            success:
+              "opacity-100 z-[9999] text-primary-500 flex items-center w-fit px-4 py-2",
+            warning:
+              "opacity-100 z-[9999] text-yellow-400 flex items-center w-fit px-4 py-2",
+            info: "opacity-100 z-[9999] bg-blue-400 flex items-center w-fit px-4 py-2",
           },
         }}
       />
